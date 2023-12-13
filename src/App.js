9430ff8fd6ace91eb3fa37db3d3fee604a601268
aljaarir <133610949+aljaarir@users.noreply.github.com>
2023-12-12 19:58:00 -0500

    App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { TodoWrapper } from './components/TodoWrapper';
 function App() {
   return (
     <div className="App">
-      <TodoWrapper  />
+      <TodoWrapper />
+      <footer style={{ marginTop: '20px', textAlign: 'center', color: '#888' }}>
+        Created by Ryan Aljaari
+      </footer>
     </div>
   );
 }
